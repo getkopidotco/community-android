1133b0e524b5184270d56571316c147cc8db4219
Andry <andry.yosua@pinteraktif.com>
2016-04-11 11:18:22 +0700

    complete start screen

---
+++ app/src/main/java/co/getkopi/community/StartActivity.java
@@ -0,0 +1,19 @@
+package co.getkopi.community;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+    public void openMainActivity(View view) {
+        Intent intent = new Intent(this, ShopActivity.class);
+        startActivity(intent);
+    }
+}
